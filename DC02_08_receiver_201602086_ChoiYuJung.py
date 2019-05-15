14cf72b4187bf2b55f5c1119acb7e22ceacf8978
choiyujung <dbwjd1094@cnu.ac.kr>
2019-05-15 20:55:04 +0900

    assignment

---
+++ DC02_08_receiver_201602086_ChoiYuJung.py
@@ -0,0 +1,27 @@
+import socket
+
+socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+port = 9800
+socket.bind(('',port))
+
+data_transferred = 0
+_data,addr = socket.recvfrom(1024)
+print("file recv start from",addr[0])
+print("File Name : ",_data.decode())
+filesize = int(_data.decode())
+print("File Size : ",filesize)
+
+data,_ = socket.recvfrom(1024)
+with open('receive file','wb') as f:
+    while data:
+        f.write(data)
+        data_transferred += len(data)
+        if filesize <= data_transferred:
+            break
+        data, _ = socket.recvfrom(1024)
+        print("current_size / total_size = ",data_transferred,"/",filesize,",",(data_transferred/filesize)*100)
+
+f.close()
+socket.close()
+print("file receive complete")
+
--- DC02_08_sender_201602086_ChoiYuJung.py
+++ DC02_08_sender_201602086_ChoiYuJung.py
@@ -1,24 +1,28 @@
 import socket
-FLAGS = None
+import os
 
 socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-ip = "192.168.31.139"
-port = 9100
-addr(ip,port)
+ip = "127.0.0.1"
+port = 9800
+addr = ip,port
 
 filename = input("input your file name : ")
 f=open(filename,"rb")
 data=f.read(1024)
 
 socket.sendto(str(os.path.getsize(filename)).encode(),addr)
+data_transferred = 0
 while data:
+    print(data)
     data_transferred += len(data)
-    data=f.read(1024)
     socket.sendto(data,addr)
     print('current / totalsize = ',data_transferred,'/',int(str(os.path.getsize(filename)).encode()),',',(data_transferred/int(str(os.path.getsize(filename)).encode())*100)
+            )
+    data = f.read(1024)
 
-print("ok")
+
 f.close()
+print("ok")
 socket.close()
 print("file_send_end")
 
